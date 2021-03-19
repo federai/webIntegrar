082d25ee1a185f54d5712a81cb4085aba829fc65
federai93 <fraimondovargas@gmail.com>
2021-03-19 12:47:54 -0300

    s

--- js/app.js
+++ js/app.js
@@ -10,3 +10,11 @@ function myFunction() {
     myNav.classList.remove("nav-colored");
   }
 }
+
+var categorie = document.getElementById("categorie1");
+console.log(categorie);
+
+// categorie.addEventListener('mouseover',color());
+function color(){
+  document.getElementById("title").innerHTML="";
+}
