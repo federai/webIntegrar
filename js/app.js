338584585dc80ba0b211fa6dd37eac4428feccca
federai93 <fraimondovargas@gmail.com>
2021-03-25 10:57:26 -0300

    avances

--- js/app.js
+++ js/app.js
@@ -9,12 +9,14 @@ function myFunction() {
     myNav.classList.add("nav-transparent");
     myNav.classList.remove("nav-colored");
   }
+
+
+  
+
+
 }
 
-var categorie = document.getElementById("categorie1");
-console.log(categorie);
-
-// categorie.addEventListener('mouseover',color());
-function color(){
-  document.getElementById("title").innerHTML="";
-}
+// var img = document.getElementById("planSites");
+// img.addEventListener ('click',()=>{
+  
+// })
